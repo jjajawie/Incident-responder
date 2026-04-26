4495b51cca6432c59ec5a41c568d2d4e91d55c3c
ykamal310 <ykamal@umich.edu>
2026-04-25 22:41:59 -0400

    added continuous monitoring with adjustable timings, added demo_api for live testing, and incident simulation

--- incident-responder.py
+++ incident-responder.py
@@ -3,6 +3,9 @@ import time
 from datetime import datetime
 import requests
 
+checking = False
+demo_api = True
+
 api_dict = {
     "GitHub": {
         "url": "https://api.github.com",
@@ -48,6 +51,16 @@ api_dict = {
         "alert_threshold": 3,
         "headers": {"Accept": "application/json"},
         "retry_count": 2
+    },
+
+     "DemoAPI": {
+        "url": "demo://manual-toggle",
+        "expected_status": [200],
+        "timeout": 0,
+        "check_interval": 5,
+        "alert_threshold": 3,
+        "headers": {},
+        "retry_count": 1
     }
     
 }
@@ -74,7 +87,7 @@ CREATE TABLE IF NOT EXISTS incidents (
 )
 """)
 #checks for incidents and logs them
-def check_for_incident(service_name, expected_status, threshold=3):
+def check_for_incident(cursor, service_name, expected_status, threshold=3):
     cursor.execute("""
         SELECT status
         FROM results
@@ -96,82 +109,166 @@ def check_for_incident(service_name, expected_status, threshold=3):
 
 success_count = 0
 
+def run_checks(log_func=print):
+    global checking
+
+    con = sqlite3.connect("log.db")
+    cursor = con.cursor()
+
+    success_count = 0
+
 #loops the check
-for service_name, config in api_dict.items():
-    url = config["url"]
-    timeout = config["timeout"]
-    expected_status = config["expected_status"]
-    retry_count = config["retry_count"]
-    headers = config["headers"]
- 
-    response = None
-    last_error = None
- 
-    for attempt in range(retry_count):
-        try:
-            response = requests.get(url, timeout=timeout, headers=headers)
-            break  # success — stop retrying
-        except requests.exceptions.RequestException as e:
-            last_error = e
-            print(f"Attempt {attempt + 1} failed for {service_name}: {e}")
- 
-    if response is not None:
-        response_time = response.elapsed.total_seconds()
- 
-        print(f"[{service_name}] Status: {response.status_code} | Response time: {response_time:.3f}s")
- 
-        cursor.execute(
-            "INSERT INTO results VALUES (?, ?, ?, ?, ?, ?)",
-            (
-                datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-                service_name,
-                url,
-                response.status_code,
-                response_time,
-                None
+    for service_name, config in api_dict.items():
+        if service_name == "DemoAPI":
+            if demo_api:
+                response_status = 200
+                response_time = 0.001
+                error_message = None
+                log_func("[DemoAPI] Status: 200 | Response time: 0.001s")
+            else:
+                response_status = None
+                response_time = None
+                error_message = "Demo API manually set to DOWN"
+                log_func("[DemoAPI] Manually set to DOWN.")
+
+            cursor.execute(
+                "INSERT INTO results VALUES (?, ?, ?, ?, ?, ?)",
+                (
+                    datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                    service_name,
+                    config["url"],
+                    response_status,
+                    response_time,
+                    error_message
+                )
             )
-        )
+            
+            
 
-        if response.status_code in expected_status:
-            success_count += 1
+            if response_status in config["expected_status"]:
+                success_count += 1
+                log_func("[DemoAPI] Saved to database.")
+            else:
+                log_func("[DemoAPI] Failure saved to database.")
 
-        print(f"[{service_name}] Saved to database.")
+            if check_for_incident(cursor, service_name, config["expected_status"], threshold=config["alert_threshold"]):
+                incident_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                incident_details = f"{service_name} failed {config['alert_threshold']} checks in a row."
 
-    else:
-        print(f"[{service_name}] All {retry_count} attempts failed.")
-        print(f"[{service_name}] Last error: {last_error}")
+                log_func(f"INCIDENT DETECTED: {incident_details}")
 
-        cursor.execute(
-            "INSERT INTO results VALUES (?, ?, ?, ?, ?, ?)",
-            (
-                datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-                service_name,
-                url,
-                None,
-                None,
-                str(last_error)
+                cursor.execute(
+                    "INSERT INTO incidents VALUES (?, ?, ?, ?)",
+                    (
+                        incident_time,
+                        service_name,
+                        "Consecutive Failures",
+                        incident_details
+                    )
+                )
+            continue
+        url = config["url"]
+        timeout = config["timeout"]
+        expected_status = config["expected_status"]
+        retry_count = config["retry_count"]
+        headers = config["headers"]
+    
+        response = None
+        last_error = None
+    
+        for attempt in range(retry_count):
+            try:
+                response = requests.get(url, timeout=timeout, headers=headers)
+                break  # success — stop retrying
+            except requests.exceptions.RequestException as e:
+                last_error = e
+                log_func(f"Attempt {attempt + 1} failed for {service_name}: {e}")
+    
+        if response is not None:
+            response_time = response.elapsed.total_seconds()
+    
+            log_func(f"[{service_name}] Status: {response.status_code} | Response time: {response_time:.3f}s")
+    
+            cursor.execute(
+                "INSERT INTO results VALUES (?, ?, ?, ?, ?, ?)",
+                (
+                    datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                    service_name,
+                    url,
+                    response.status_code,
+                    response_time,
+                    None
+                )
             )
-        )
-        print(f"[{service_name}] Failure saved to database.")
 
-    if check_for_incident(service_name, expected_status, threshold=config["alert_threshold"]):
-        incident_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        incident_details = f"{service_name} failed {config['alert_threshold']} checks in a row."
+            if response.status_code in expected_status:
+                success_count += 1
 
-        print(f"INCIDENT DETECTED: {incident_details}")
+            log_func(f"[{service_name}] Saved to database.")
 
-        cursor.execute(
-            "INSERT INTO incidents VALUES (?, ?, ?, ?)",
-            (
-                incident_time,
-                service_name,
-                "Consecutive Failures",
-                incident_details
+        else:
+            log_func(f"[{service_name}] All {retry_count} attempts failed.")
+            log_func(f"[{service_name}] Last error: {last_error}")
+
+            cursor.execute(
+                "INSERT INTO results VALUES (?, ?, ?, ?, ?, ?)",
+                (
+                    datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                    service_name,
+                    url,
+                    None,
+                    None,
+                    str(last_error)
+                )
             )
-        )
-    time.sleep(config["check_interval"])
+            log_func(f"[{service_name}] Failure saved to database.")
 
-con.commit()
-con.close()
- 
-print(f"\nSuccessful checks: {success_count} / {len(api_dict)}")
+        if check_for_incident(cursor, service_name, expected_status, threshold=config["alert_threshold"]):
+            incident_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            incident_details = f"{service_name} failed {config['alert_threshold']} checks in a row."
+
+            log_func(f"INCIDENT DETECTED: {incident_details}")
+
+            cursor.execute(
+                "INSERT INTO incidents VALUES (?, ?, ?, ?)",
+                (
+                    incident_time,
+                    service_name,
+                    "Consecutive Failures",
+                    incident_details
+                )
+            )
+        #time.sleep(config["check_interval"])
+
+    con.commit()
+    con.close()
+    log_func(f"\nSuccessful checks: {success_count} / {len(api_dict)}")
+
+def start_monitoring(interval=5, log_func=print):
+    global checking
+
+    checking = True
+
+    while checking:
+        run_checks(log_func)
+
+        log_func(f"Waiting {interval} seconds before next check...")
+
+        for _ in range(interval):
+            if not checking:
+                break
+            time.sleep(1)
+
+    log_func("Monitoring stopped.")
+
+
+def stop_monitoring():
+    global checking
+    checking = False
+
+def set_demo_api_status(is_up):
+    global demo_api
+    demo_api = is_up
+
+if __name__ == "__main__":
+    start_monitoring()
